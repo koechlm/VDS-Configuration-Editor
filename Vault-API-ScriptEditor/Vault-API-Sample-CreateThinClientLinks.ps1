3a9850737876e6b341e088da0634302242fd1f7a
Markus Koechl <markus.koechl@autodesk.com>
2023-11-08 17:50:47 +0100

    new sample to create Thin Client links for all supported entity types

---
+++ Vault-API-ScriptEditor/Vault-API-Sample-CreateThinClientLinks.ps1
@@ -0,0 +1,77 @@
+﻿#region disclaimer
+	#===============================================================================
+	# PowerShell script sample														
+	# Author: Markus Koechl															
+	# Copyright (c) Autodesk 2021													
+	#																				
+	# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER     
+	# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES   
+	# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.    
+	#===============================================================================
+#endregion
+
+#region ConnectToVault
+
+		# NOTE - click licensing v7 requires to copy AdskLicensingSDK_7.dll, AdskIdentitySDK.config, and AdskIdentitySDK.dll to PowerShell execution folder C:\Windows\System32\WindowsPowerShell\v1.0 before Powershell runtime starts
+
+		[System.Reflection.Assembly]::LoadFrom('C:\Program Files\Autodesk\Autodesk Vault 2024 SDK\bin\x64\Autodesk.Connectivity.WebServices.dll')
+		$serverID = New-Object Autodesk.Connectivity.WebServices.ServerIdentities
+			$serverID.DataServer = "<servername>"
+			$serverID.FileServer = "<servername>"
+		$VaultName = "PDMC-Sample"
+		$UserName = "CAD Admin"
+		$password = ""
+		#Select license type by licensing agent enum "Client" (=Named User) "Server" (= (legacy) Multi-User) or "None" (=readonly access) or "Token" (Autodesk ID)
+		$licenseAgent = [Autodesk.Connectivity.WebServices.LicensingAgent]::Client
+		
+		$cred = New-Object Autodesk.Connectivity.WebServicesTools.UserPasswordCredentials($serverID, $VaultName, $UserName, $password, $licenseAgent)
+		$vault = New-Object Autodesk.Connectivity.WebServicesTools.WebServiceManager($cred)
+
+		#region ExecuteInVault
+
+			# in order to run the sample code, you need to identify a file linked to an item and controlled by a change order
+			# ToDo: change the file path in line 36 accordingly
+
+			#get file master	
+			$filePaths = @("$/Designs/SR-0006/Discharge Chute/01-0745.iam")	
+			$file = $vault.DocumentService.FindLatestFilesByPaths($filePaths)[0]
+			#create TC link
+			$TcFileMasterLink = "http://" + $serverID.DataServer + "/AutodeskTC/" + $VaultName + "/explore/file/" + $file.MasterId
+			Start-Process $TcFileMasterLink
+			
+			#get historical file iterations
+			$files = @()
+			$files += $vault.DocumentService.GetFilesByMasterId($file.MasterId)
+			#create TC links for all historical versions
+			$files | ForEach-Object{
+				$TcFileVersionLink = "http://" + $serverID.DataServer + "/AutodeskTC/" + $VaultName + "/explore/fileversion/" + $_.Id
+				Start-Process $TcFileVersionLink
+			}
+
+			#get item of the file
+			$item = $vault.ItemService.GetItemsByFileId($file.Id)[0]
+			#create TC link
+			$TcItemMasterLink = "http://" + $serverID.DataServer + "/AutodeskTC/" + $VaultName + "/items/item/" + $item.MasterId
+			Start-Process $TcItemMasterLink
+
+			#get historical item iterations
+			$items = @()
+			$items += $vault.ItemService.GetItemHistoryByItemMasterId($item.MasterId, "All")
+			#create TC links for all historical item versions
+			$items | ForEach-Object{
+				$TcItemVersionLink = "http://" + $serverID.DataServer + "/AutodeskTC/" + $VaultName + "/items/itemversion/" + $_.Id
+				Start-Process $TcItemVersionLink
+			}
+
+			#get change order
+			$changeOrder = $vault.ChangeOrderService.GetChangeOrderFilesByFileMasterId($file.MasterId)[0] 
+			#create TC link of CO
+			$TcChangeOrderLink = "http://" + $serverID.DataServer + "/AutodeskTC/" + $VaultName + "/changeorders/changeorder/" + $changeOrder.ChangeOrder.Id
+			Start-Process $TcChangeOrderLink
+
+		#endregion ExecuteInVault
+		
+		$vault.Dispose() #don't forget to release the connection, to return the (server) license you also can log out: $cred.SignOut($vault.AuthService, $vault.WinAuthService)
+
+
+#endregion ConnectToVault
