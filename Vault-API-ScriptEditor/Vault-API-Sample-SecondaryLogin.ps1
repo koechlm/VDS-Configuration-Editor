bb6f43ea6d1507312a90e53a1f726a89ec6f8897
Markus Koechl <markus.koechl@autodesk.com>
2024-11-07 09:54:43 +0100

    added sample for secondary login

--- Vault-API-ScriptEditor/Vault-API-Sample-SecondaryLogin.ps1
+++ Vault-API-ScriptEditor/Vault-API-Sample-SecondaryLogin.ps1
@@ -2,7 +2,7 @@
 	#===============================================================================
 	# PowerShell script sample														
 	# Author: Markus Koechl															
-	# Copyright (c) Autodesk 2021													
+	# Copyright (c) Autodesk 2024													
 	#																				
 	# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER     
 	# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES   
@@ -18,18 +18,37 @@
 		$serverID = New-Object Autodesk.Connectivity.WebServices.ServerIdentities
 			$serverID.DataServer = "<ServerName or IP>"
 			$serverID.FileServer = "<ServerName or IP>"
-		$VaultName = "<Name of Vault>"
-		$UserName = "<User Name>"
-		$password = "<Password>"
+		$VaultName = "<Vault Name>"
+		$UserName = "<User Login Name>"
+		$password = ""
 		#Select license type by licensing agent enum "Client" (=Named User) "Server" (= (legacy) Multi-User) or "None" (=readonly access) or "Token" (Autodesk ID)
 		$licenseAgent = [Autodesk.Connectivity.WebServices.LicensingAgent]::Client
-		
+
+		# the primary login
 		$cred = New-Object Autodesk.Connectivity.WebServicesTools.UserPasswordCredentials($serverID, $VaultName, $UserName, $password, $licenseAgent)
 		$vault = New-Object Autodesk.Connectivity.WebServicesTools.WebServiceManager($cred)
 
 		#region ExecuteInVault
 
-			#query vault, download files, etc....		
+		#check accessibility for the current user and switch to secondary login
+
+			$file = ($vault.DocumentService.FindLatestFilesByPaths(@("$/Designs/CAD Admins Only/01-1084.iam")))[0]
+			
+			if ($file.Locked -eq $true) {
+				#establish secondary login and check locked state again
+				$secondaryUserName = "<Another Login Name>"
+				$secondaryPassword = ""
+
+				$cred2 = New-Object Autodesk.Connectivity.WebServicesTools.UserPasswordCredentials($serverID, $VaultName, $secondaryUserName, $secondaryPassword, $licenseAgent)
+				$vault2 = New-Object Autodesk.Connectivity.WebServicesTools.WebServiceManager($cred2)
+
+				$file2 = ($vault2.DocumentService.FindLatestFilesByPaths(@("$/Designs/CAD Admins Only/01-1084.iam")))[0]
+				#do something with the file
+
+				#release the secondary connection
+				$vault2.Dispose()
+			}
+
 			
 		#endregion ExecuteInVault
 		
